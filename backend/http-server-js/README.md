642754354caf7c5ba89f0e542c9b5982538eb9eb
dan <dan.du@leidos.com>
2023-12-06 00:16:18 -0500

    test

---
+++ backend/http-server-js/README.md
@@ -0,0 +1,16 @@
+### generate an application 
+```
+npm install -g express-generator@4
+express foo
+```
+
+### install dependencies:
+ ```
+npm install
+```
+
+### run the app:
+```
+DEBUG=foo:* npm start
+```
+
---
+++ backend/http-server-js/foo/test/app.test.js
@@ -0,0 +1,18 @@
+var app = require('../app')
+var request = require('supertest')
+require("dotenv").config();
+
+describe('GET /', () => {
+  test('GET /', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+  })
+})
+
+describe('GET /users', () => {
+  test('GET /', async () => {
+    const res = await request(app).get('/users');
+   
+    expect(res.statusCode).toBe(200);
+  })
+})
